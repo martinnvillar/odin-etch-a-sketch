751eef49792d8e7ca1b375c6bd76576934a6478c
martinnvillar <martinnnvillar@icloud.com>
2023-05-03 20:49:48 -0300

    update main.js, added styled grid pixels, sliderValue works as it increment/decrement

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-const grid = document.querySelector('gridContainer');
+// buttons, input and grid
+
+const gridSelector = document.querySelector('.grid');
+const colorPicker = document.querySelector('.colorPicker');
+let sliderValue = document.querySelector('.sliderValue');
+const blackBtn = document.querySelector('#blackBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const slider = document.querySelector('#slider');
+
+let pixel = '';
+let gridSize = 16;
+
+function createGrid (screenSize) {
+    for (let i = 0; i < screenSize ** 2; i++) { 
+        pixel = document.createElement('div');
+        pixel.style.border = '1px solid black';
+        pixel.classList.add('pixel');
+        pixel.style.backgroundColor = 'white';
+        gridSelector.appendChild(pixel);
+    };
+
+    gridSelector.style.gridTemplateColumns = `repeat(${screenSize}, auto)`;
+    gridSelector.style.gridTemplateRows = `repeat(${screenSize}, auto)`;
+};
+
+createGrid(gridSize);
+
+slider.addEventListener('input', function () {
+    let val = document.getElementById('slider').value;
+    sliderValue.textContent = val;
+});
